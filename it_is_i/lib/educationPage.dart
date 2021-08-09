02f0c6a03f1740f5ae2de046f158939b47968fea
Sichangi prime <sichangifelix23@gmail.com>
2021-08-09 20:16:30 +0300

    Worked on the card for the education page

--- it_is_i/lib/educationPage.dart
+++ it_is_i/lib/educationPage.dart
@@ -14,8 +14,38 @@ class Education extends StatelessWidget {
         backgroundColor: Color(0xff03141b),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text('THIS IS THE  PAGE CONTAINING MY EDUCATION HISTORY'),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Color(0xff03141b),
+                border: Border.all(
+                  color: Colors.black87,
+                  width: 2.0,
+                  style: BorderStyle.solid,
+                ),
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      //logo section,
+                      Column(
+                        children: [
+                          Text('data'),
+                          Text('data'),
+                        ],
+                      )
+                    ],
+                  ),
+                  Text('Honors received from it'),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
