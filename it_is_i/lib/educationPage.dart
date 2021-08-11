44b9d13b66cfafb566683988a34c400dd7491ca2
Sichangi prime <sichangifelix23@gmail.com>
2021-08-11 11:23:48 +0300

    I extracted the widget into a reusable class

--- it_is_i/lib/educationPage.dart
+++ it_is_i/lib/educationPage.dart
@@ -18,49 +18,78 @@ class Education extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                height: 250.0,
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color(0xff03141b),
-                      Color(0xff152238),
-                      Color(0xff192841),
-                      Color(0xff1C2E4A)
-                    ],
-                    stops: [0.5, 0.8, 1.0, 1.0],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        //logo section
-                        Container(
-                          child: new Image.asset('images/KU logo.png'),
-                        ),
-                        Container(
-                          child: Column(
-                            children: [
-                              Text('data'),
-                              Text('data'),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    Text('Honors received from it'),
-                  ],
-                ),
-              ),
+              EducationCard(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class EducationCard extends StatelessWidget {
+  final String institutionName, yearOfStudy, honorsReceived;
+
+  EducationCard(
+      {required this.institutionName,
+      required this.yearOfStudy,
+      required this.honorsReceived});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250.0,
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff03141b),
+            Color(0xff152238),
+            Color(0xff192841),
+            Color(0xff1C2E4A)
+          ],
+          stops: [0.5, 0.8, 1.0, 1.0],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                //logo section
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    child: new Image.asset('images/Kamusinga.jpg'),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Kenyatta University'),
+                        Text('2019-2023'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: Text('Bachelor of Science Degree in Biology'),
+            ),
+          ),
+        ],
       ),
     );
   }
