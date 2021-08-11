8c231fc9adf633a3d9007454547da09cc4a5a956
Sichangi prime <sichangifelix23@gmail.com>
2021-08-11 10:45:50 +0300

    Worked on a card gradient

--- it_is_i/lib/educationPage.dart
+++ it_is_i/lib/educationPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
@@ -14,38 +15,51 @@ class Education extends StatelessWidget {
         backgroundColor: Color(0xff03141b),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-              padding: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                color: Color(0xff03141b),
-                border: Border.all(
-                  color: Colors.black87,
-                  width: 2.0,
-                  style: BorderStyle.solid,
-                ),
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      //logo section,
-                      Column(
-                        children: [
-                          Text('data'),
-                          Text('data'),
-                        ],
-                      )
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 250.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xff03141b),
+                      Color(0xff152238),
+                      Color(0xff192841),
+                      Color(0xff1C2E4A)
                     ],
+                    stops: [0.5, 0.8, 1.0, 1.0],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
-                  Text('Honors received from it'),
-                ],
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        //logo section
+                        Container(
+                          child: new Image.asset('images/KU logo.png'),
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Text('data'),
+                              Text('data'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Text('Honors received from it'),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
